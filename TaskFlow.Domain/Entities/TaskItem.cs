2fcd1ece47dbf9fbf6976b3fc26839cda2f2184a
Jesus Llanos <jesullafer@hotmail.com>
2026-03-19 19:15:37 +0000

    chore: initialize TaskFlow base structure

---
+++ TaskFlow.Domain/Entities/TaskItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Domain.Entities;
+
+public class TaskItem
+{
+    public Guid Id { get; private set; }
+    public string Title { get; private set; }
+    public string? Description { get; private set; }
+    public bool IsCompleted { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public TaskItem(string title, string? description)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("El título es obligatorio.");
+
+        Id = Guid.NewGuid();
+        Title = title;
+        Description = description;
+        IsCompleted = false;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void Update(string title, string? description)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("El título es obligatorio.");
+
+        Title = title;
+        Description = description;
+    }
+
+    public void MarkAsCompleted()
+    {
+        IsCompleted = true;
+    }
+}
