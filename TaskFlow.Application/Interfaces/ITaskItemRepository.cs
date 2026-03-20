18449e663947708815dddba511f29c5bbbf9c25d
Jesus Llanos <jesullafer@hotmail.com>
2026-03-20 10:51:00 +0000

    feat: add task DTOs and repository contract

---
+++ TaskFlow.Application/Interfaces/ITaskItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskFlow.Domain.Entities;
+
+namespace TaskFlow.Application.Interfaces
+{
+    public interface ITaskItemRepository
+    {
+        Task<List<TaskItem>> GetAllAsync();
+        Task<TaskItem?> GetByIdAsync(Guid id);
+        Task AddAsync(TaskItem taskItem);
+    }
+}
